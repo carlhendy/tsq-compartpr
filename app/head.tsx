0a08763987ad7a404b54840d9024e5aa56a5cd1b
carlhendy <hello@carlhendy.com>
2025-09-09 14:46:09 +1000

    Delete app/head.tsx

--- app/head.tsx
+++
@@ -1,8 +0,0 @@
-export default function Head() {
-  return (
-    <>
-      <title>Compare Google Store Ratings - Bulk Upload</title>
-      <meta name="description" content="Benchmark Ecommerce Stores by Google’s Public Quality Signals. Free Bulk Upload." />
-    </>
-  );
-}
