2bf3aedc9267e29a303d48fdb6225c4ca3de7727
carlhendy <hello@carlhendy.com>
2025-09-15 16:16:23 +1000

    Add files via upload

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,9 @@
 import "./globals.css";
 
-export const metadata={title:"Compare Google Store Ratings (Bulk & Free)"};
+export const metadata={
+  title:"Compare Google Store Ratings – Bulk & Free",
+  icons: {
+    icon: "https://audits.com/wp-content/uploads/2024/05/Favicon-Solid-Blue__Square.png"
+  }
+};
 export default function RootLayout({children}:{children:React.ReactNode}){return(<html lang="en"><body>{children}</body></html>)}
